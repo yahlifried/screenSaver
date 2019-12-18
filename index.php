00c7ab61114ac2c16c088bce2981a5599dce907d
yahlifried <48161112+yahlifried@users.noreply.github.com>
2019-12-18 08:06:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -43,79 +43,79 @@
 			}
 			10%{
 				color: #ff9933;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff9933;
 			}
 			15%{
 				color: #ff6600;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff6600;
 			}
 			20%{
 				color: #ff5050;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff5050;
 			}
 			25%{
 				color: #ff0066;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff0066;
 			}
 			30%{
 				color: #ff3399;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff3399;
 			}
 			35%{
 				color: #ff00ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #ff00ff;
 			}
 			40%{
 				color: #cc33ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #cc33ff;
 			}
 			45%{
 				color: #9966ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #9966ff;
 			}
 			50%{
 				color: #6666ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #6666ff;
 			}
 			55%{
 				color: #3366ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #3366ff;
 			}
 			60%{
 				color: #0066ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #0066ff;
 			}
 			65%{
 				color: #0099ff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #0099ff;
 			}
 			70%{
 				color: #00ccff;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #00ccff;
 			}
 			75%{
 				color: #33cccc;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #33cccc;
 			}
 			80%{
 				color: #00ffcc;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #00ffcc;
 			}
 			85%{
 				color: #00ff99;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #00ff99;
 			}
 			90%{
 				color: #00ff00;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #00ff00;
 			}
 			95%{
 				color: #33cc33;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #33cc33;
 			}
 			100%{
 				color: #009900;
-				text-shadow:0px 0px 150px #ffcc00;
+				text-shadow:0px 0px 150px #009900;
 			}
 		}
 	</style>
